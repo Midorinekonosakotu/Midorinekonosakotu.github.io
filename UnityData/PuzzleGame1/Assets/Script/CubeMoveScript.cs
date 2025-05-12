06f97cbdcec2369af1ecac2689bc0d7b334cb5ff
Midorinekonosakotu <midorineko10801@gmail.com>
2025-05-12 23:41:10 +0900

    PuzzleGameの初期設定&マウスドラッグ実装

---
+++ UnityData/PuzzleGame1/Assets/Script/CubeMoveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CubeMoveScript : MonoBehaviour
+{
+    // マウスで掴んだゲームオブジェクトの中心座標と、マウスカーソルの座標の差分を格納する変数
+    private Vector3 offset;
+
+    // マウスでゲームオブジェクトをクリックしたときに呼び出されるメソッドです。offset変数に、マウスカーソルとゲームオブジェクトの中心座標の差分を格納します。
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    // マウスでゲームオブジェクトをドラッグしているときに呼び出されるメソッドです。GetMouseWorldPos()メソッドを使ってマウスカーソルの座標をワールド座標に変換し、offset変数を足してゲームオブジェクトを移動させます。
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+    }
+
+
+    // マウスカーソルの座標をスクリーン座標からワールド座標に変換するメソッドです。Input.mousePositionでスクリーン座標を取得し、Camera.main.ScreenToWorldPoint(mousePos)でワールド座標に変換して返します。
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
