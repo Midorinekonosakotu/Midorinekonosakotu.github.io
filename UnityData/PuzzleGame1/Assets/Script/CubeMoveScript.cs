1487987593f97beac977db459ec63965dbf6370b
Midorinekonosakotu <midorineko10801@gmail.com>
2025-05-20 21:38:30 +0900

    パズルフレームの作成途中

--- UnityData/PuzzleGame1/Assets/Script/CubeMoveScript.cs
+++ UnityData/PuzzleGame1/Assets/Script/CubeMoveScript.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CubeMoveScript : MonoBehaviour
 {
     // マウスで掴んだゲームオブジェクトの中心座標と、マウスカーソルの座標の差分を格納する変数
     private Vector3 offset;
+    private bool cubeMove = true;
+
+    private void Update()
+    {
+        
+    }
 
     // マウスでゲームオブジェクトをクリックしたときに呼び出されるメソッドです。offset変数に、マウスカーソルとゲームオブジェクトの中心座標の差分を格納します。
     void OnMouseDown()
     {
-        offset = gameObject.transform.position - GetMouseWorldPos();
+        if(cubeMove)
+        {
+            offset = gameObject.transform.position - GetMouseWorldPos();
+        }
     }
 
     // マウスでゲームオブジェクトをドラッグしているときに呼び出されるメソッドです。GetMouseWorldPos()メソッドを使ってマウスカーソルの座標をワールド座標に変換し、offset変数を足してゲームオブジェクトを移動させます。
     void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPos() + offset;
+        if(cubeMove)
+        {
+            transform.position = GetMouseWorldPos() + offset;
+        }
     }
-
 
     // マウスカーソルの座標をスクリーン座標からワールド座標に変換するメソッドです。Input.mousePositionでスクリーン座標を取得し、Camera.main.ScreenToWorldPoint(mousePos)でワールド座標に変換して返します。
     private Vector3 GetMouseWorldPos()
